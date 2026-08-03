c1bb2d96fc82451fa3e358da57be6573b4fa9f99
nguyenvanhoang3001-hub <nguyenvanhoang3001@gmail.com>
2026-08-03 10:29:39 +0700

    feat: connect course-service to MySQL database

--- course-service/src/main/java/vn/edu/crs/courseservice/controller/CourseController.java
+++ course-service/src/main/java/vn/edu/crs/courseservice/controller/CourseController.java
@@ -3,30 +3,22 @@ package vn.edu.crs.courseservice.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.edu.crs.courseservice.entity.Course;
+import vn.edu.crs.courseservice.repository.CourseRepository;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
 
+    private final CourseRepository courseRepository;
+
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
     @GetMapping
-    public List<Map<String, Object>> getMockCourses() {
-        return List.of(
-                Map.of(
-                        "id", 1,
-                        "tenMonHoc", "Lap trinh Java co ban",
-                        "soTinChi", 3,
-                        "soChoToiDa", 40,
-                        "soChoConLai", 12
-                ),
-                Map.of(
-                        "id", 2,
-                        "tenMonHoc", "Co so du lieu",
-                        "soTinChi", 4,
-                        "soChoToiDa", 35,
-                        "soChoConLai", 0
-                )
-        );
+    public List<Course> getCourses() {
+        return courseRepository.findAll();
     }
 }
---
+++ course-service/src/main/java/vn/edu/crs/courseservice/repository/CourseRepository.java
@@ -0,0 +1,7 @@
+package vn.edu.crs.courseservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import vn.edu.crs.courseservice.entity.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
